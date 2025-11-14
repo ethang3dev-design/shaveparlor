ce8bf9cfcb7cec59b91a44ce5b6303df63c9f449
ethang3dev-design <ethang3.dev@gmail.com>
2025-11-14 15:24:59 -0500

    Create sheets.js

---
+++ functions/sheets.js
@@ -0,0 +1,54 @@
+exports.handler = async (event, context) => {
+  const GOOGLE_URL = "https://script.google.com/macros/s/AKfycbypSO_TIVome8l-jdIqOHjXLmfaf0Mi_8tQukNzIG9BrC8gH0sd3bcnqm4AaeSmQa7Y9A/exec";
+
+  try {
+    // ----- GET (list appointments) -----
+    if (event.httpMethod === "GET") {
+      const r = await fetch(GOOGLE_URL + "?action=list");
+      const data = await r.json();
+
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
+        body: JSON.stringify(data)
+      };
+    }
+
+    // ----- POST (add appointment) -----
+    if (event.httpMethod === "POST") {
+      const body = JSON.parse(event.body);
+
+      const r = await fetch(GOOGLE_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      });
+
+      const data = await r.json();
+
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
+        body: JSON.stringify(data)
+      };
+    }
+
+    // ----- NON SUPPORTED METHOD -----
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed"
+    };
+  }
+  catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: String(err) })
+    };
+  }
+};
